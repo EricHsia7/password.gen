f5b035db52838a29172fa49e035b7b0220322a3f
EricHsia7 <54091924+EricHsia7@users.noreply.github.com>
2020-11-28 17:48:43 +0800

    Update addtohome.js

--- addtohome.js
+++ addtohome.js
@@ -1,7 +1,14 @@
+var getUrlString = location.href;
+var url = new URL(getUrlString);
 function getRandom(min,max) {
     return Math.floor(Math.random()*(max-min+1))+min;
 }
+if("0" === url.searchParams.get('h') ) {
+}
+else {
 $('.addhome').delay(getRandom(5000,10000)).animate({bottom: 25 + 'px'},333);
+}
 function closeaddhome() {
 $('.addhome').animate({bottom: -100 + 'px'},333);
+location.replace('?h=0');
 }
